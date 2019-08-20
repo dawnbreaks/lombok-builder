e5b6a20449c54819207081d2244fd58b1715898c
lubinwang <308181687@qq.com>
2019-08-20 17:09:49 +0800

    test

--- src/main/java/lombok/test/grpcclient/GrpcClient.java
+++ src/main/java/lombok/test/grpcclient/GrpcClient.java
@@ -1,11 +1,10 @@
 package lombok.test.grpcclient;
 
-import lombok.test.grpcclient.ClientSettings.ClientConfig;
 import io.grpc.stub.AbstractStub;
 import lombok.Builder;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-import lombok.extern.slf4j.Slf4j;
+import lombok.test.grpcclient.ClientSettings.ClientConfig;
 
 /*
  *author: lubin
@@ -14,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Builder
 @Accessors(fluent = true)
-@Slf4j
 @ToString(exclude = {"stub"})
 public class GrpcClient<B extends AbstractStub<B>> {
     public static final int DEFAULT_DNS_MIN_TTL_SECONDS = 5;
